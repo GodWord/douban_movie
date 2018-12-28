db58bfa23b149ed3c5888d3b1741003e71b2382b
vFeng <z1914007838@outlook.com>
2018-12-28 22:49:42 +0800

    fix-优化程序启动入口

--- app.py
+++ app.py
@@ -1,12 +1,12 @@
 # -*- coding:utf-8 -*-
+import io
+import sys
+
 from selenium import webdriver
 
-from jobs.douban_short import run, get_browser
+from jobs.douban_short import run
 from utils.logger import Logger
 
-browser = webdriver.Chrome()
 if __name__ == '__main__':
     Logger()
-    browser = get_browser()
     run()
-    browser.quit()
