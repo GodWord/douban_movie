97707fad8af630b07cd1e6ce49cc908975542ffa
vFeng <z1914007838@outlook.com>
2018-12-29 14:29:03 +0800

    feature-在爬虫完成之后开始生成词云

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 # -*- coding:utf-8 -*-
 
 from jobs.douban_short import run
+from jobs.word_count import word_count
 from utils.logger import Logger
 
 if __name__ == '__main__':
     Logger()
     run()
+    word_count
